6e1ed29ef44fb1ff9dbfc33832d34c26756433c2
Mazin Idris <mazin1231@hotmail.com>
2024-04-09 15:22:33 -0600

    shadcn validation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,14 +1,19 @@
 import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants/index'
 
 
 const AddTransformationTypePage = ({ params: { type }}: SearchParamProps) => {
   const transformation = transformationTypes[type];
   return (
+    <>
     <Header 
     title={transformation.title}
     subtitle={transformation.subTitle}
     />
+
+    <TransformationForm/>
+    </>
   )
 }
 
