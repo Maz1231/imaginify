961c6415257b01622604954c2f768814f6f40f53
Mazin Idris <mazin1231@hotmail.com>
2024-04-09 20:17:02 -0600

    image upload

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { useToast} from "@/components/ui/use-toast"
-import { CldUploadWidget} from "next-cloudinary"
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "@/node_modules/next/dist/shared/lib/get-img-props";
+import Image from "@/node_modules/next/image";
+import { CldImage, CldUploadWidget} from "next-cloudinary"
 
 type MediaUploaderProps = {
     onValueChange: (value: string) => void;
-    setImage: React.dispatch<any>;
+    setImage: React.Dispatch<any>;
     publicId: string;
     image: any;
     type: string
@@ -19,6 +24,15 @@ const MediaUploader = ({
     const { toast } = useToast()
 
     const onUploadSuccessHandler = (result: any) => {
+        setImage((prevState: any) => ({
+            ...prevState, 
+            publicId: result?.info?.public_id,
+            width: result?.info?.width,
+            height: result?.info?.height,
+            secureURL: result?.info?.secure_url
+        }))
+
+        onValueChange(result?.info?.public_id)
         toast({
             title: 'Image Uploaded successfully',
             description: '1 credit was deducted from your account',
@@ -57,12 +71,30 @@ const MediaUploader = ({
                 <h3 className="h3-bold text-dark-600">Original</h3>
                 {publicId ? (
                     <>
-                    HERE IS THE IMAGE
+                    <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                    <CldImage
+                    width={getImageSize(type, image,"width")}
+                    height={getImageSize(type, image,"height")}
+                    src={publicId}
+                    alt="image"
+                    sizes={"(max-width: 767px) 100vw, 50vw"}
+                    placeholder={dataUrl as PlaceholderValue}
+                    className="media-uploader_cldImage"
+                    />
+                    </div>
                     </>
                 ): (
-                    <div>
-                        HERE IS NO IMAGE
-
+                    <div className="media-uploader_cta" onClick={() => open()}>
+                        <div className="media-uploader_cta-image">
+                        <Image
+                        src="/assets/icons/add.svg"
+                        alt="Add Image"
+                        width={24}
+                        height={24}
+                        />
+                        
+                        </div>
+                        <p className="p-14-medium">Click here to upload image</p>
                     </div>
                 )}
             </div>
